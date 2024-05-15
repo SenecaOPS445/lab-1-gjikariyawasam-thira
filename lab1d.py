ffbb70d2380b106f6e36c3f74358133e633ce923
Jihan.Kariyawasam <gjikariyawasam-thira@myseneca.ca>
2024-05-14 21:34:04 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+x = 10
+y = 2
+z = 5
+
+sum = x + y * z
+
+x1 = str(x)
+y1 = str(y)
+z1 = str(z)
+
+sum1 = str(sum)
+
+print ( x1 +  ' + ' + y1 + ' * ' + z1 + ' = ' + sum1)
+
